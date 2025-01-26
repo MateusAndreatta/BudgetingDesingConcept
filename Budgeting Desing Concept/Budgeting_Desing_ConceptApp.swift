30b82a029b058ec33c6cafb9d01c0b66ec0f7517
Mateus Andreatta <mateus-henrique2001@hotmail.com>
2025-01-26 13:52:37 -0300

    Initial Commit

---
+++ Budgeting Desing Concept/Budgeting_Desing_ConceptApp.swift
@@ -0,0 +1,17 @@
+//
+//  Budgeting_Desing_ConceptApp.swift
+//  Budgeting Desing Concept
+//
+//  Created by Mateus Andreatta on 26/01/25.
+//
+
+import SwiftUI
+
+@main
+struct Budgeting_Desing_ConceptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Budgeting Desing Concept/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Budgeting Desing Concept
+//
+//  Created by Mateus Andreatta on 26/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
